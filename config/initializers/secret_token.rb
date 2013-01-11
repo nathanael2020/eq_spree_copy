59edf4072fead9fce5f1235e8aaa22e7ca793fcc
Nathanael Miksis <nathanael.miksis@gmail.com>
2013-01-11 14:02:48 -0800

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EqSpree::Application.config.secret_token = 'a3f7d13b850304fd44a4b8643e5bfab01431d516efbc5386ff2c853ff63b85fe28b4f6478e1af461082629e5b9bc1559accddc3c56b0339730c0a276821db3aa'
