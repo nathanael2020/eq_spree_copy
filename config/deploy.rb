f43641acf9742e4fea8cc9df0f54eb63d127b796
Nathanael Miksis <nathanael.miksis@gmail.com>
2013-01-17 18:20:18 -0800

    latest

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,25 +1,81 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "eq_spree_copy"
+set :repository,  "git://github.com/nathanael2020/eq_spree_copy.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+role :web, "184.106.81.10"                          # Your HTTP server, Apache/etc
+role :app, "184.106.81.10"                          # This may be the same as your `Web` server
+role :db,  "184.106.81.10", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
-# if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
+set :user, "nathanael"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+default_run_options[:pty] = true
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+
+
+# Don't show so much! (Log levels: IMPORTANT, INFO, DEBUG, TRACE, MAX_LEVEL)
+logger.level = Capistrano::Logger::DEBUG
+ 
+# Since we're using pty, load the path ourselves
+set :default_environment, {
+  "PATH" => "/home/nathanael/.rbenv/shims:/home/nathanael/.rbenv/bin:$PATH"
+}
+ 
+ 
+namespace :deploy do
+  desc "Zero-downtime restart of Unicorn"
+  task :restart, roles: :app, :except => { :no_release => true } do
+    run "kill -s USR2 `cat /tmp/unicorn.eq_spree_copy.pid`"
+  end
+ 
+  desc "Start Unicorn"
+  task :start, roles: :app, :except => { :no_release => true } do
+    run "cd #{current_path} ; bundle exec unicorn_rails -c config/unicorn.rb -D"
+  end
+ 
+  desc "Stop Unicorn"
+  task :stop, roles: :app, :except => { :no_release => true } do
+    run "kill -s QUIT `cat /tmp/unicorn.eq_spree_copy.pid`"
+  end 
+ 
+ 
+  namespace :assets do
+ 
+    desc <<-DESC
+      Run the asset precompilation rake task. You can specify the full path \
+      to the rake executable by setting the rake variable. You can also \
+      specify additional environment variables to pass to rake via the \
+      asset_env variable. The defaults are:
+ 
+        set :rake,      "rake"
+        set :rails_env, "production"
+        set :asset_env, "RAILS_GROUPS=assets"
+ 
+      * only runs if assets have changed (add `-s force_assets=true` to force precompilation)
+    DESC
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      # Only precompile assets if any assets changed
+      # http://www.bencurtis.com/2011/12/skipping-asset-compilation-with-capistrano/
+      begin
+
+      from = source.next_revision(current_revision)
+      
+      rescue 
+        err_no = true
+       end
+      if err_no || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ | wc -l").to_i > 0
+        run %Q{cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile}
+      
+#      if fetch(:force_assets, false) || capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ app/assets/ lib/assets/ | wc -l").to_i > 0
+        # Just like original: https://github.com/capistrano/capistrano/blob/master/lib/capistrano/recipes/deploy/assets.rb
+#        run "cd #{latest_release} && #{rake} RAILS_ENV=#{rails_env} #{asset_env} assets:precompile"
+      else
+        logger.info "Skipping asset pre-compilation because there were no asset changes"
+      end
+    end
+ 
+  end
+ 
+end
